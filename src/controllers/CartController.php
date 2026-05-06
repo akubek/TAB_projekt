c25c9d9080a99f2ea62aa8db530ff49e95184cfc
akubek <artur.kubek.96@gmail.com>
2026-05-06 21:53:50 +0200

    fixed product attribute display in cart

--- src/controllers/CartController.php
+++ src/controllers/CartController.php
@@ -13,6 +13,7 @@ class CartController
         $cart = isset($_COOKIE['cart']) ? json_decode($_COOKIE['cart'], true) : [];
         $items = [];
         $totalPrice = 0;
+        $colorsDict = require BASE_PATH . '/config/colors.php';
 
         if (!empty($cart)) {
             $variantIds = array_keys($cart);
@@ -22,12 +23,27 @@ class CartController
                 $qty = $cart[$v['variant_id']];
                 $unitPrice = $v['base_price'] + $v['price_modifier'];
                 $subtotal = $unitPrice * $qty;
+                $rawAttributes = json_decode($v['attributes'], true);
+                $formattedAttributes = [];
+
+                if (is_array($rawAttributes)) {
+                    foreach ($rawAttributes as $key => $value) {
+                        if ($key === 'color') {
+                            $friendlyColorName = $colorsDict[$value]['name'] ?? $value;
+                            $formattedAttributes['Kolor'] = $friendlyColorName;
+                        } elseif ($key === 'size') {
+                            $formattedAttributes['Rozmiar'] = $value;
+                        } else {
+                            $formattedAttributes[ucfirst($key)] = $value;
+                        }
+                    }
+                }
 
                 $items[] = [
                     'variant_id' => $v['variant_id'],
                     'product_id' => $v['product_id'],
                     'name' => $v['product_name'],
-                    'attributes' => json_decode($v['attributes'], true),
+                    'attributes' => $formattedAttributes,
                     'image' => json_decode($v['images'], true)[0] ?? 'https://placehold.co/100x100',
                     'unit_price' => $unitPrice,
                     'quantity' => $qty,
